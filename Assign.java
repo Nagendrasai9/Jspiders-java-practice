a0f97daf47003878c7292999430d804a3a589ffe
Nagendrasai9 <69496169+Nagendrasai9@users.noreply.github.com>
2023-11-17 09:46:41 +0530

    Add files via upload

---
+++ Assign.java
@@ -0,0 +1,23 @@
+class Assign 
+{
+	public static void main(String[] args) 
+	{
+		char a='1';
+		if((a>='A' && a<='Z')||(a>='a' && a<='z'))
+		{
+			if(a=='a' ||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+			{
+				System.out.println("Vowel");
+				
+			}
+			else
+			{
+				System.out.println("Consonent");
+			}
+		}
+		else
+		{
+			System.out.println("Not a alphabet");
+		}
+	}
+}
